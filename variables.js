f8ff30baab0c6306b06d21693d457752dd34de14
Ryan Wells <rswells2@gmail.com>
2024-01-26 10:23:01 -0700

    finished arrays

--- variables.js
+++ variables.js
@@ -1,17 +1,20 @@
 //////////////////PROBLEM 1////////////////////
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
-//CODE HERE 
+const myName = "Ryan Wells"
+// console.log(myName)  "this checks the const myName"
 
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
-//CODE HERE
+const faveNum = 7
+// console.log(faveNum) "this checks the const faveNum"
 
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
-//CODE HERE
+const lovesCode = true
+// console.log(lovesCode) "this checks the const lovesCode"
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -20,11 +23,20 @@
   If it's not, console.log('not lucky 13').
 */
 
-//CODE HERE
+if(faveNum === 13){
+  console.log("lucky 13!") // Should expect to see this if the number is equal to 13
+}
+  else{
+    console.log("not lucky 13") // Should expect to see this if the number is any thing other than 13
+  }
 
 //////////////////PROBLEM 5////////////////////
 /*
   Write a for loop that prints out your faveNum 5 times.
 */
 
-//CODE HERE
+for (i = 0; i < 5; i++){
+  console.log(faveNum)
+  console.log("--------------")
+}
+
